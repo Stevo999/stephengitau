2b5ce7600486b37ef9f272f2550c7b36fa55cbfe
Steevo999 <gitaustephen84@gmail.com>
2023-07-24 19:58:33 +0300

    second commit

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-<script>mailjs.init('eYjp_YD9y7MUq-vql');</script>
-  document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     const contactForm = document.getElementById("contactForm");
-
+  
+    // Step 1: Initialize MailJS with your public API key
+    mailjs.init('eYjp_YD9y7MUq-vql');
+  
     contactForm.addEventListener("submit", function(event) {
       event.preventDefault();
-
+  
       // Get the form data
       const formData = new FormData(contactForm);
-
-      // Send the email using MailJS
+  
+      // Step 2: Send the email using MailJS
       mailjs.send('template_t451dfq', {
-        to_name: 'Recipient Name', // Replace with the recipient's name or email
+        to_name: 'gitaustephen84@gmail.com', // Replace with the recipient's name or email
         from_name: formData.get('name'),
         from_email: formData.get('email'),
         message: formData.get('message'),
@@ -26,5 +28,5 @@
       });
     });
   });
-
+  
 
