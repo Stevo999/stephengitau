956310af0335208238daf167346740ac726db952
Steevo999 <gitaustephen84@gmail.com>
2023-07-24 19:10:30 +0300

    second commit

--- script.js
+++ script.js
@@ -1,41 +1,31 @@
-<script>
-  mailjs.init('eYjp_YD9y7MUq-vql');
-</script>
+<script> mailjs.init('eYjp_YD9y7MUq-vql');</script>
 
-document.addEventListener("DOMContentLoaded", function() {
+  document.addEventListener("DOMContentLoaded", function() {
     const contactForm = document.getElementById("contactForm");
 
     contactForm.addEventListener("submit", function(event) {
-        event.preventDefault();
+      event.preventDefault();
 
-        // Get the form data
-        const formData = new FormData(contactForm);
+      // Get the form data
+      const formData = new FormData(contactForm);
 
-        // Send the form data to the server-side script for processing
-        sendFormData(formData);
-    });
-});
-
-function sendFormData(formData) {
-    // Replace "submit_contact_form.php" with the URL of your server-side script that handles form submission
-    const url = "submit_contact_form.php";
-
-    fetch(url, {
-        method: "POST",
-        body: formData
-    })
-    .then(response => {
-        if (response.ok) {
-            alert("Message sent successfully!");
-            // Optionally, you can clear the form fields after successful submission
-            const contactForm = document.getElementById("contactForm");
-            contactForm.reset();
-        } else {
-            alert("Error sending message. Please try again later.");
-        }
-    })
-    .catch(error => {
+      // Send the email using MailJS
+      mailjs.send('template_t451dfq', {
+        to_name: 'Recipient Name', // Replace with the recipient's name or email
+        from_name: formData.get('name'),
+        from_email: formData.get('email'),
+        message: formData.get('message'),
+      }).then(function (response) {
+        // Email sent successfully, show a success message and optionally clear the form fields
+        alert("Message sent successfully!");
+        contactForm.reset();
+        console.log('Email sent successfully!', response);
+      }).catch(function (error) {
+        // An error occurred while sending the email, show an error message
         alert("Error sending message. Please try again later.");
-        console.error("Error:", error);
+        console.error('Error sending email:', error);
+      });
     });
-}
+  });
+
+
