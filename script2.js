6baf4fb9bad1631dc888a870ae7756d00c701c50
Steevo999 <gitaustephen84@gmail.com>
2023-08-17 11:00:03 +0300

    second commit

--- script2.js
+++ script2.js
@@ -7,8 +7,38 @@ document.addEventListener("DOMContentLoaded", function() {
     minimizeButton.addEventListener("click", function() {
         navList.classList.toggle("minimized");
     });
+
+    // Toggle minimized class on scroll
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 0) {
+            minimizeButton.classList.add("minimized");
+        } else {
+            minimizeButton.classList.remove("minimized");
+        }
+    });
 });
 
+function sendFormData(formData) {
+    const url = "submit_contact_form.php";
+
+    fetch(url, {
+        method: "POST",
+        body: formData
+    })
+    .then(response => {
+        if (response.ok) {
+            alert("Message sent successfully!");
+            const contactForm = document.getElementById("contactForm");
+            contactForm.reset();
+        } else {
+            alert("Error sending message. Please try again later.");
+        }
+    })
+    .catch(error => {
+        alert("Error sending message. Please try again later.");
+        console.error("Error:", error);
+    });
+}
 function sendFormData(formData) {
     const url = "submit_contact_form.php";
 
